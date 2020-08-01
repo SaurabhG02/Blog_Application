b1969a9ec01a88b4faa22729828cc925e548c16b
SaurabhG02 <59133760+SaurabhG02@users.noreply.github.com>
2020-08-01 11:45:51 +0530

    1st commit

---
+++ testproj/blog/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path, include
+from blog import views
+
+urlpatterns = [
+    path('',views.HomeView.as_view(), name='home'),
+    path('aboutus/',views.AboutUsView.as_view(), name='aboutus'),
+    path('category/<int:id>',views.CategoryView.as_view(), name='category'),
+    path('blog/<str:slug>',views.PostView.as_view(), name='blog'),
+    path('createpost/',views.PostCreateView.as_view(), name='createpost'),
+    path('editpost/<str:slug>',views.PostUpdateView.as_view(), name='editpost'),
+    # path('deletepost/<str:slug>',views.PostDeleteView.as_view(), name='deletepost'),
+    path('search/', views.search, name='search')
+]
